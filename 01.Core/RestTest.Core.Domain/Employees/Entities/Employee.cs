4437e7d1104cc0f8d39d30467c218e6a97325851
H.Shamekhi <h.shamekhi@dotin.ir>
2023-01-03 16:02:24 +0330

    Models

---
+++ 01.Core/RestTest.Core.Domain/Employees/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace RestTest.Core.Domain.Employees.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string NationalCode { get; set; }
+        public ICollection<Note> Notes { get; set; }
+    }
+}
